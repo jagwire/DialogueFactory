da3b47c48dc36b352a7a1e70f90306e7e609659a
Ryan Babiuch <pympnotiq@gmail.com>
2014-04-28 10:29:44 -0500

    Initial project commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular
+  .module('dialogueFactoryApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
